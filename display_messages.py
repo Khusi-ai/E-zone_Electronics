d025d48254c9c1c2bcb02a0ba99d3e806611fcd9
Khusi-ai <gkhusi561@gmail.com>
2024-07-25 20:20:13 +0545

    Add files via upload

---
+++ display_messages.py
@@ -0,0 +1,74 @@
+def welcome_message():
+    
+    #printing welcome message
+    print("\n")
+    print("\t\t\t\t\t\t\t\t\t\t E-zone Electronics ")
+    print("\t\t\t\t\t\t\t\t\t   Dillibazar, Kathmandu, Nepal ")
+    print("\t\t\t\t\t\t\t\t\t\t     9876543675")
+    print("\n")
+    print("\t\t\t*----------------------------------------------------------------------------------------------------------------------------------------*")
+    print("\t\t\t\t\t\t\t\t\tis the no.1 shop for laptops in town")
+    print("\t\t\t\t\t\t\t*--------------------------------------------------------------------- *")
+    print("\n")
+    print("\t\t\t\t\t\t Welcome!  We hope you find what you're looking for and that you enjoy your stay.")
+    print("  =====================================================================================================================================================================================")
+    print("\n")
+
+def select_option():
+    print("\t|\tEnter 1 to Purchase.\t|")
+    print("\t|\tEnter 2 to Sell.    \t|")
+    print("\t|\tEnter 3 to Exit.    \t|")
+    print("\n")
+    print("--------------------------------------------")
+    
+
+def fill_information():
+    
+    print("\n")
+    print("Fill out the information below: ")
+    print("\n")
+    print("--------------------------------------------")
+
+def table():
+    print("-------------------------------------------------------------------------------------------------------------------------------------")
+    print("ID\tName \t\t|\t   Brand\t|\t   Price\t|\tQuantity|\t \tCPU\t|\t   Graphics ")
+    print("-------------------------------------------------------------------------------------------------------------------------------------")
+
+def exception():
+     print("\n------------------------------")
+     print("Input Invalid.Enter the number!")
+     print("--------------------------------")
+
+def invalid_error():
+    print("\n------------------------")
+    print("ID entered is not valid!")
+    print("Please enter valid id")
+    print("--------------------------")
+    print("\n")
+
+def quantity_unavailable():
+    print("\n------------------------------------------")
+    print("The quantity is not availabel for this id.")
+    print("-------------------------------------------")
+    print("\n")
+
+def main():
+    print("\n\t  ---------------------------")
+    print("\t  Please enter only integers!")
+    print("\t  ---------------------------")
+    print("\n")
+
+def purchase():
+    print("--------------------------------------------\n")
+    print("*You are ready to purchase*")
+
+def sell():
+    print("--------------------------------------------\n")
+    print("*You are ready sell the product*")
+
+def exit():
+    print("\n\t\tEXITED!")
+    print("\tThank you for visiting us.")
+
+def display():
+    print("\t\t\t***\n")
